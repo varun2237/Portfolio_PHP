189bcf65e79c5ebc720803e03f0c2b870eed5d19
Varun-Gupta <varungupta2237@gmail.com>
2024-02-04 22:05:29 +0530

    First

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "portfolio1"; 
+
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name );
+
+?>
